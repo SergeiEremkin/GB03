6934cb82bb2a14e62ee3f739df69d0b4730db29d
Sergei Eremkin <ffr@list.ru>
2021-12-13 21:35:49 +0300

    Первый тест

---
+++ App_01/App_01/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_01
+{
+
+    
+
+       
+
+
+    }
+    internal class Program
+    {
+    public static void PrintArrayBack(int[] arr)
+    {
+
+        for (int i = arr.Length -1 ; i >=0; i--)
+        {
+            Console.Write(arr[i]);
+        }
+
+    }
+    static void Main(string[] args)
+        {
+        var arr = new int[] { 2, 4, 5, 6, 8 };
+
+        foreach (var a in arr)
+
+            Console.Write(a);
+
+        Console.WriteLine();
+
+        PrintArrayBack(arr);
+
+        Console.ReadLine();
+        }
+    }
+
+
+
+
