5c0a181f62eadd4e953fb26839ce064f1deff3ef
Sergei Eremkin <ffr@list.ru>
2021-12-22 18:44:09 +0300

    Add Homework2

---
+++ HW_02/HW_02/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                      
+            var testAccount = new BankAccount {Balance = 200, Type = Type.EUR };
+
+            Console.WriteLine("Счет №" + "" + testAccount.Number + "\nБаланс " + testAccount.Balance + " " + testAccount.Type);
+
+            Console.WriteLine();
+
+            BankAccount.WhithDrow(1300,testAccount);
+
+            Console.WriteLine();
+
+            BankAccount.WhithDrow(100, testAccount);
+
+            Console.WriteLine();
+
+            BankAccount.PutOnTheAccount(1000, testAccount);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HW_02/HW_02/bankAccount.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_02
+{
+    enum Type
+    {
+        RUB,
+        EUR,
+        USD
+    };
+
+    class BankAccount
+    {
+        private int _number;
+
+        private int _balance;
+
+        private Type _type;
+
+        // public static int _rndNumber = NumberGen(); Тестовая переменная для рэндомного счета
+
+        public int Number
+        {
+            get
+            {
+                return _number;
+            }
+
+            set
+            {
+                _number = value;
+            }
+        }
+
+        public int Balance
+        {
+            get
+            {
+                return _balance;
+            }
+
+            set
+            {
+                _balance = value;
+            }
+
+        }
+
+        public Type Type
+        {
+            get
+            {
+                return _type;
+            }
+
+            set
+            {
+                _type = value;
+            }
+        }
+
+        public BankAccount() : this(0)
+        {
+            Number = NumberGen();
+        }
+
+        public BankAccount(int number) : this(1, 2)
+        {
+            Number = NumberGen();
+        }
+
+        public BankAccount(int number, int balance) : this(2, 3, Type.RUB)
+        {
+            Number = NumberGen();
+        }
+
+        public BankAccount(int number, int balance, Type type)
+        {
+            Number = NumberGen();
+            Balance = balance;
+            Type = type;
+
+        }
+
+        public static int NumberGen()
+
+        {
+
+            var rnd = new Random();
+
+            return rnd.Next(1000, 1000000);
+
+        }
+
+        public static void WhithDrow(int summ, BankAccount bankAccount)
+        {
+            if ((bankAccount.Balance) >= summ)
+            {
+                bankAccount.Balance = bankAccount.Balance - summ;
+
+                Console.WriteLine($"Сумма {summ} {bankAccount.Type} снята. Баланс составляет: {bankAccount.Balance} {bankAccount.Type}");
+            }
+
+            else
+            {
+                Console.WriteLine("На балансе не достаточно средств");
+            }
+
+        }
+
+        public static void PutOnTheAccount(int summ, BankAccount bankAccount)
+        {
+            bankAccount.Balance = bankAccount.Balance + summ;
+
+            Console.WriteLine($"Пополнение на сумму: {summ} {bankAccount.Type}. Баланс составляет: {bankAccount.Balance} {bankAccount.Type} ");
+        }
+
+    }
+}
