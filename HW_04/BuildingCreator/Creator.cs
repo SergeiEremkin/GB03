bb540bfdd4ce5de2f50c0180597b57323d66eaba
Sergei Eremkin <ffr@list.ru>
2022-01-02 15:21:38 +0300

    Add Homework4

---
+++ HW_04/BuildingCreator/Creator.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_04
+{
+      public class Creator : Building
+    {
+
+        private static Building _building;
+        private static Hashtable _data = new Hashtable();
+
+        public static Building Building
+
+        {
+            get
+
+            {
+
+                return _building;
+
+            }
+
+            set
+
+            {
+                _building = value;
+            }
+        }
+
+        private static Hashtable Data
+
+        {
+            get
+
+            {
+
+                return _data;
+
+            }
+
+            set
+
+            {
+                _data = value;
+            }
+        }
+
+
+        private Creator ()
+
+        {
+            
+
+        }
+
+        public static void CreateBuild(Building building)
+
+        {
+            Building = building;
+
+            Data.Add(building.ID, building);
+        }
+
+
+        public static void DataInfo()
+
+        {
+
+            foreach ( DictionaryEntry d in Data)
+
+            {
+                Console.WriteLine("Key = {0}, Value = {1}", d.Key, d.Value);
+
+            }
+
+        }
+
+        public static void Delete(int key)
+
+        {
+
+            try
+
+            {
+                if (Data.ContainsKey(key))
+
+                {
+                    Data.Remove(key);
+
+                }
+
+            }
+
+            catch
+
+            {
+                Exception exception = new Exception("Такого элемента нет ");
+
+            }
+               
+
+        }
+        
+
+       
+    }
+}
---
+++ HW_04/Example/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_04
+{
+
+     class Program
+    {
+
+        static void Main(string[] args)
+        {
+             
+            
+            Creator.CreateBuild(new Building { Apartment = 25, Floor = 2, Height = 10, Front = 45, });
+
+            Creator.CreateBuild(new Building { Apartment = 25, Floor = 2, Height = 10, Front = 45, });
+
+            Creator.CreateBuild(new Building { Apartment = 25, Floor = 2, Height = 10, Front = 45, });
+
+            Creator.CreateBuild(new Building { Apartment = 25, Floor = 2, Height = 10, Front = 45, });
+
+            Creator.DataInfo();
+
+            Creator.Delete(2);
+
+            Console.WriteLine("=====================================================================");
+            
+            Creator.DataInfo();
+            
+            
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
